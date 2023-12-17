4b7ebe34fcffff3241dc0f182eae35ad8b38cdd4
Minh Quân <scott.do.goldenowl@gmail.com>
2023-12-17 11:37:35 +0700

    lesson 2

---
+++ script.js
@@ -0,0 +1,40 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// while thì sẽ kiểm tra trước và thực hiện sau
+// let i = 3
+while (i > 0) {
+    // i = i - 1
+    // console.log(i);
+}
+
+// do-while làm trước kiểm tra sau
+// let j = 10
+do {
+    // j = j - 2
+    // console.log(j);
+} while (j > 0);
+
+// vòng lặp for: Khởi tạo, điều kiện, bước nhảy
+for (let index = 0; index <= 20; index++) {
+    // console.log(index);
+}
+
+// Sử dụng vòng lặp in ra màn hình 
+// các số chẵn bé hơn 10
+
+for (let index = 0; index < 10; index++) {
+    if (index % 2 == 0) {
+        console.log(index);
+    }
+}
+
+let i = 0
+while (i<10) {
+    if (i % 2) {
+        console.log(i);
+    }
+   i++ 
+}
+
